50faee84b8215330be6e14daf4ed7bd47f37c591
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2024-05-13 20:21:10 +0600

    jwt added  commit

---
+++ README.md
@@ -0,0 +1,102 @@
+
+# ProdutPulse
+
+
+Welcome to Craftify Creations, your destination for exploring the diverse world of painting and drawing! Visit our website to unleash your creativity and discover a wide range of artistic expressions. 
+
+
+# ProdutPulse  Backend and Fronted Given Below:  
+
+**Live Site URL:** [ProdutPulse Server](https://craftify-creations-server.vercel.app)
+and
+**Live Site URL:** [ProdutPulse Client](https://craftify-creations.web.app/)
+ 
+
+
+
+## Website Preview:
+ 
+
+<p float="left">
+  <img src="https://i.postimg.cc/cJtfc4zG/craftify.png" width="200" />
+  <img src="https://i.postimg.cc/HsLzyHTd/web2.jpg" width="200" /> 
+  <img src="https://i.postimg.cc/k4cN2YZ2/web3.jpg" width="200" />
+</p>
+<p float="left">
+  <img src="https://i.postimg.cc/T13VgqMC/web4.jpg" width="200" />
+  <img src="https://i.postimg.cc/1RvcPTwT/web5.jpg" width="200" /> 
+  <img src="https://i.postimg.cc/0jXpfM9x/web6.jpg" width="200" />  
+
+## Painting and Drawing:
+
+Explore the endless possibilities of painting and drawing with ProdutPulse. From traditional techniques to digital artistry, there's something here for every artist.
+
+
+## Features:
+
+- **Landscape Painting**: Immerse yourself in the beauty of nature with our stunning mountain view canvases and forest-based landscape paintings.
+  
+- **Portrait Drawing**: Bring your loved ones to life with our exquisite pencil sketch portraits, ink outline portraits, and acrylic portrait canvases.
+  
+- **Watercolour Painting**: Explore the delicate art of watercolor with our floral watercolor creations and lifelike animal watercolor portraits.
+  
+- **Oil Painting**: Experience the richness of oil paints through our realistic oil portraits and captivating abstract oil paintings.
+  
+- **Charcoal Sketching**: Dive into the world of monochrome with our figure charcoal sketches and intricate architectural charcoal drawings.
+  
+- **Cartoon Drawing**: Let your imagination run wild with our playful comic strip drawings and dynamic superhero cartoon designs.
+
+
+## Getting Started
+To visit our store and experience the wonderful world of coffee, simply drop by our location at [insert address]. We look forward to welcoming you!
+
+## Contact Us
+If you have any questions, feedback, or inquiries, feel free to reach out to us at [insert contact information].
+
+
+## Installation
+1. Clone the repository: `git clone https://github.com/programming-hero-web-course-4/B9A10-client-side-Faysalgreen35`
+2. Navigate to the project directory: `cd  craftify-creations-client`
+3. Install dependencies: `npm install`
+4. Start the development server: `npm run dev`
+5. Start the deploy server: `npm run build`
+
+## Usage
+Once the development server is running, visit `http://localhost:5173` in your web browser to access ProdutPulse. Users can explore property listings, gather information, and connect with sellers or agents for further inquiries.
+
+## Challenges Task
+For this project, I have implemented the following npm packages to address specific challenges:
+- **Swiper Slider**: I used the Swiper package to create interactive sliders for showcasing property images and other visual content.
+- **AOS Package**: I used the AOS package to add animations to my Home, enhancing the user experience and visual appeal.
+- **React Hook Form**: I implemented React Hook Form to manage and validate complex forms efficiently, providing a seamless experience for users when submitting inquiries or contact forms.
+
+- **React-simple-typewriter**: Enhance your web development skills by participating in our React-simple-typewriter challenge. Create captivating typewriter animations to add an extra flair to your projects.
+  
+- **React-tooltip**: Dive into the world of frontend design with our React-tooltip challenge. Learn how to implement interactive tooltips to improve user experience and engagement on your websites.
+
+
+
+## Contributing
+Contributions are welcome! If you'd like to contribute to ProdutPulse, please fork the repository, create a new branch, commit your changes, and open a pull request. Please ensure to follow the existing code style and conventions.
+
+## Credits
+- [React](https://reactjs.org/)
+- [React Router](https://reactrouter.com/)
+
+ 
+## npm Packages Used:
+
+- **React-tooltip**: A versatile tooltip library for React applications, perfect for enhancing user interactions and providing helpful hints.
+  
+- **React-simple-typewriter**: An easy-to-use React component that simulates typewriter text animations, adding a dynamic touch to your web projects.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+
+## Get Inspired, Get Creative!
+
+ProdutPulse is not just a website; it's a platform for artists and art enthusiasts alike to connect, inspire, and create. Join us on this journey of artistic exploration and let your imagination soar!
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config()
 const app = express();
@@ -8,15 +11,53 @@ const port = process.env.PORT || 5000;
 
 
 
-// miidleware  written here
+// miidleware  written here  http://localhost:5173
 const corsConfig = {
-  origin: '*',
+  origin: [
+    'http://localhost:5173',
+    'https://product-pulse-7aeac.web.app',
+    'https://product-pulse-7aeac.firebaseapp.com'
+  ],
   credentials: true,
   optionSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }
 app.use(cors(corsConfig))
 app.use(express.json());
+app.use(cookieParser());
+
+
+const logger = async (req, res, next) => {
+  console.log('called:', req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log('value of token in middleware', token)
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //error
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    //if token is valid then it would be docoded
+
+    console.log('value in the token', decoded)
+
+    req.user = decoded
+
+    next()
+  })
+
+}
+
 
 
 
@@ -31,6 +72,13 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const cookieOption = {
+  httpOnly:true,
+  semeSite:process.env.NODE_ENV === 'production' ? 'none': 'strict',
+  secure: process.env.NODE_ENV === 'production' ? true : false,
+ }
+
 async function run() {
   try {
     const queryCollection = client.db('productPulseDB').collection('queries');
@@ -38,13 +86,36 @@ async function run() {
 
     //   const categoryCollection = client.db('craftifycreationsDB').collection('category');
 
+    //auth related api  create token
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1d' })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
 
-    app.get('/query', async (req, res) => {
+        })
+        .send({ success: true });
+    })
+//clear token
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('logging out ', user);
+      res.clearCookie('token', { ...cookieOption, maxAge: 0 }).send({ success: true });
+    })
+
+
+
+    app.get('/query',logger, async (req, res) => {
       const cursor = queryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
-    app.get('/recommendation', async (req, res) => {
+    app.get('/recommendation',logger,verifyToken, async (req, res) => {
       const cursor = recommendationCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -62,28 +133,28 @@ async function run() {
     })
 
     //  Get a single query data from db using id
-    
+
 
     app.get('/recommendation/:id', async (req, res) => {
       const id = req.params.id;
       let query;
-  
+
       // Check if the id is a valid ObjectId
       query = { $or: [{ queries_id: new ObjectId(id) }, { queries_id: id }] };
-  
+
       try {
-          const results = await recommendationCollection.find(query).toArray();
-          if (results.length > 0) {
-              res.send(results);
-          } else {
-              res.status(404).send("No recommendations found for the specified id");
-          }
+        const results = await recommendationCollection.find(query).toArray();
+        if (results.length > 0) {
+          res.send(results);
+        } else {
+          res.status(404).send("No recommendations found for the specified id");
+        }
       } catch (error) {
-          console.error("Error:", error);
-          res.status(500).send("Internal Server Error");
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
       }
-  });
-  
+    });
+
 
 
 
@@ -142,86 +213,86 @@ async function run() {
     });
 
 
-    
-// delete query
-app.delete('/query/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { $or: [{ _id: new ObjectId(id) }, { _id: id }] };
-  try {
-    const result = await queryCollection.deleteOne(query);
-    if (result.deletedCount === 1) {
-      res.status(200).json({ success: true, message: "Query deleted successfully." });
-    } else {
-      res.status(404).json({ success: false, message: "Query not found." });
-    }
-  } catch (error) {
-    console.error("Error deleting query:", error);
-    res.status(500).json({ success: false, message: "Failed to delete the query." });
-  }
-});
 
-//delete recommendation
+    // delete query
+    app.delete('/query/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { $or: [{ _id: new ObjectId(id) }, { _id: id }] };
+      try {
+        const result = await queryCollection.deleteOne(query);
+        if (result.deletedCount === 1) {
+          res.status(200).json({ success: true, message: "Query deleted successfully." });
+        } else {
+          res.status(404).json({ success: false, message: "Query not found." });
+        }
+      } catch (error) {
+        console.error("Error deleting query:", error);
+        res.status(500).json({ success: false, message: "Failed to delete the query." });
+      }
+    });
 
-app.delete('/recommendation/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { $or: [{ _id: new ObjectId(id) }, { _id: id }] };
-  try {
-    // Get the recommendationData first
-    const recommendationData = await recommendationCollection.findOne(query);
-    if (!recommendationData) {
-      return res.status(404).json({ success: false, message: "Recommendation not found." });
-    }
+    //delete recommendation
 
-    const result = await recommendationCollection.deleteOne(query);
-    if (result.deletedCount === 1) {
-      // Update the recommendation count for the corresponding query
-      await queryCollection.updateOne(
-        // { _id: new ObjectId(recommendationData.queries_id) },
-        { $or: [{ _id: new ObjectId(recommendationData.queries_id) }, { _id: recommendationData.queries_id }] },
-        { $inc: { recommendationcount: -1 } } // Decrease count by 1
-      );
-      res.status(200).json({ success: true, message: "Recommendation deleted successfully." });
-    } else {
-      res.status(404).json({ success: false, message: "Recommendation not found." });
-    }
-  } catch (error) {
-    console.error("Error deleting recommendation:", error);
-    res.status(500).json({ success: false, message: "Failed to delete the recommendation." });
-  }
-});
+    app.delete('/recommendation/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { $or: [{ _id: new ObjectId(id) }, { _id: id }] };
+      try {
+        // Get the recommendationData first
+        const recommendationData = await recommendationCollection.findOne(query);
+        if (!recommendationData) {
+          return res.status(404).json({ success: false, message: "Recommendation not found." });
+        }
 
- 
+        const result = await recommendationCollection.deleteOne(query);
+        if (result.deletedCount === 1) {
+          // Update the recommendation count for the corresponding query
+          await queryCollection.updateOne(
+            // { _id: new ObjectId(recommendationData.queries_id) },
+            { $or: [{ _id: new ObjectId(recommendationData.queries_id) }, { _id: recommendationData.queries_id }] },
+            { $inc: { recommendationcount: -1 } } // Decrease count by 1
+          );
+          res.status(200).json({ success: true, message: "Recommendation deleted successfully." });
+        } else {
+          res.status(404).json({ success: false, message: "Recommendation not found." });
+        }
+      } catch (error) {
+        console.error("Error deleting recommendation:", error);
+        res.status(500).json({ success: false, message: "Failed to delete the recommendation." });
+      }
+    });
 
-//update query
-app.put('/updateQuery/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { $or: [{ _id: new ObjectId(id) }, { _id: id }] };
 
-  const updatedQuery = req.body;
-  const updateQueries = {
-      $set: {
+
+    //update query
+    app.put('/updateQuery/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { $or: [{ _id: new ObjectId(id) }, { _id: id }] };
+
+      const updatedQuery = req.body;
+      const updateQueries = {
+        $set: {
           productname: updatedQuery.productname,
           productbrand: updatedQuery.productbrand,
           productimageurl: updatedQuery.productimageurl,
           querytitle: updatedQuery.querytitle,
           boycottingreasondetails: updatedQuery.boycottingreasondetails
-      }
-  };
+        }
+      };
 
-  try {
-      const result = await queryCollection.updateOne(query, updateQueries);
-      if (result.modifiedCount > 0) {
+      try {
+        const result = await queryCollection.updateOne(query, updateQueries);
+        if (result.modifiedCount > 0) {
           res.status(200).json({ success: true, message: "Query updated successfully." });
-      } else {
+        } else {
           res.status(404).json({ success: false, message: "Query not found." });
+        }
+      } catch (error) {
+        console.error("Error updating query:", error);
+        res.status(500).json({ success: false, message: "Failed to update the query." });
       }
-  } catch (error) {
-      console.error("Error updating query:", error);
-      res.status(500).json({ success: false, message: "Failed to update the query." });
-  }
-});
+    });
 
- 
+
 
     // Save a recommend data in db
 
