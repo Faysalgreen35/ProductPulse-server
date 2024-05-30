5f2f876b47c4365f3d9cb05efb15ffe7beda48a0
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2024-05-30 14:03:11 +0600

    dashboard api added commit

--- index.js
+++ index.js
@@ -83,8 +83,11 @@ async function run() {
   try {
     const queryCollection = client.db('productPulseDB').collection('queries');
     const recommendationCollection = client.db('productPulseDB').collection('recommendations');
+    const userCollection = client.db('productPulseDB').collection('users');
 
     //   const categoryCollection = client.db('craftifycreationsDB').collection('category');
+
+
 
     //auth related api  create token
     app.post('/jwt', logger, async (req, res) => {
@@ -109,6 +112,81 @@ async function run() {
     })
 
 
+     // use verify admin after verifytoken
+     const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+
+      }
+      next();
+    }
+
+
+
+    // user related api 
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+
+    
+    //users api
+    app.get('/users',  async (req, res) => {
+
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
+
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);;
+    })
+
+//query collect
 
     app.get('/query', logger, async (req, res) => {
       const cursor = queryCollection.find();
@@ -356,6 +434,24 @@ async function run() {
 
       const result = await queryCollection.insertOne(queryData)
       res.send(result)
+    })
+
+    
+    //stats or analytics
+    app.get('/user-stats', async(req,res)=>{
+      const users = await userCollection.estimatedDocumentCount();
+      const query = await queryCollection.estimatedDocumentCount();
+      const recommendation = await recommendationCollection.estimatedDocumentCount();
+      
+      
+
+     
+      res.send({
+        users,
+        query,
+        recommendation,
+         
+      })
     })
 
 
